cf3c9b84e6f1a76384205d337b2601314f5bb346
Y-Yes <Aonse@proton.me>
2024-05-07 12:54:15 +0300

    update V0.0.0.2

--- main.cpp
+++ main.cpp
@@ -1,25 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace sf;
+using namespace std;
 
-int main(){
-	//Creating the window
-	RenderWindow window(VideoMode(1000, 800), "Tuff");
-	//While the window is open
-	while(window.isOpen()){
-	Event event;
-	Texture texture;
+int main() {
+    RenderWindow window(VideoMode(1000, 800), "Tuff");
 
-	if(!texture.loadFromFile("tree.png")) //Checks if the file exists
-		return EXIT_FAILURE;
-	Sprite sprite(texture);
-		while (window.pollEvent(event)){
-			if(event.type==sf::Event::Closed){
-				window.close(); //If you close the window stop the game
-			}
-		}
-	window.clear();
-	window.draw(sprite); //Draws the tree
-	window.display();
-	}
-	return EXIT_SUCCESS;
+    vector<Texture> textures;
+    for (int i = 1; i <= 12; ++i) {
+        Texture texture;
+        if (!texture.loadFromFile("tree" + to_string(i) + ".png")) {
+            return EXIT_FAILURE;
+        }
+        textures.push_back(texture);
+    }
+
+    Sprite sprite(textures[0]);
+    int currentTextureIndex = 0;
+    int clickCount = 0;
+    int neededclicks = 3;
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            } else if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    if (event.mouseButton.x > window.getSize().x / 2) {
+                        ++clickCount;
+                        if (clickCount >= neededclicks) {
+                            currentTextureIndex = (currentTextureIndex + 1) % textures.size();
+                            sprite.setTexture(textures[currentTextureIndex]);
+                            clickCount = 0;
+			    neededclicks++;
+                        }
+                    }
+                }
+            }
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 }
