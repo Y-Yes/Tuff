985065a994b5279f75c7b313cf4cff84f610c4f6
Ygnas1 <160159917+Ygnas1@users.noreply.github.com>
2024-05-21 14:43:17 +0300

    Update Form1.cs

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,4 +1,5 @@
-using System.Windows.Forms.VisualStyles;
+using System;
+using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
@@ -9,31 +10,33 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonExit_Click(object sender, EventArgs e)
         {
-
-
             this.Close();
-
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void buttonPlay_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (checkBox1.Checked == true)
+            if (checkBoxTerms.Checked)
             {
-                Game GameForm = new Game();
-                GameForm.Show();
+                Game gameForm = new Game();
+                gameForm.Show();
+                this.Hide(); // Hide the main form when Play is clicked
             }
+            else
+            {
+                MessageBox.Show("You must accept the terms and conditions to play.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void checkBoxTerms_CheckedChanged(object sender, EventArgs e)
+        {
+            // Additional logic for checkbox state change can be added here
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // Initialization logic when the form loads can be added here
         }
     }
 }
