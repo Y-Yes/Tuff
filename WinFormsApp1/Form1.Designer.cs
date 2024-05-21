5a2fc873e8c14a0f22397bc2964ceb535fd64250
Ygnas1 <160159917+Ygnas1@users.noreply.github.com>
2024-05-21 14:43:01 +0300

    Update Design Form1.Designer.cs

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace WinFormsApp1
+namespace WinFormsApp1
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,72 +15,108 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            checkBox1 = new CheckBox();
-            SuspendLayout();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.checkBoxTerms = new System.Windows.Forms.CheckBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+
             // 
-            // button1
+            // panelMain
             // 
-            button1.BackColor = Color.Red;
-            button1.Location = new Point(511, 298);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Exit";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            this.panelMain.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMain.Controls.Add(this.labelTitle);
+            this.panelMain.Controls.Add(this.checkBoxTerms);
+            this.panelMain.Controls.Add(this.buttonPlay);
+            this.panelMain.Controls.Add(this.buttonExit);
+            this.panelMain.Location = new System.Drawing.Point(50, 50);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(700, 350);
+            this.panelMain.TabIndex = 4;
+
             // 
-            // button2
+            // buttonExit
             // 
-            button2.BackColor = Color.Lime;
-            button2.Location = new Point(142, 298);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 2;
-            button2.Text = "Play";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(220, 53, 69);
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonExit.ForeColor = System.Drawing.Color.White;
+            this.buttonExit.Location = new System.Drawing.Point(400, 250);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(100, 50);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+
             // 
-            // checkBox1
+            // buttonPlay
             // 
-            checkBox1.AutoSize = true;
-            checkBox1.BackColor = Color.FromArgb(255, 192, 192);
-            checkBox1.Location = new Point(230, 248);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(302, 19);
-            checkBox1.TabIndex = 3;
-            checkBox1.Text = "Accept the terms and conditions and privacy policy?";
-            checkBox1.UseVisualStyleBackColor = false;
-            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            this.buttonPlay.BackColor = System.Drawing.Color.FromArgb(40, 167, 69);
+            this.buttonPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlay.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonPlay.ForeColor = System.Drawing.Color.White;
+            this.buttonPlay.Location = new System.Drawing.Point(200, 250);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(100, 50);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = false;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+
+            // 
+            // checkBoxTerms
+            // 
+            this.checkBoxTerms.AutoSize = true;
+            this.checkBoxTerms.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBoxTerms.ForeColor = System.Drawing.Color.White;
+            this.checkBoxTerms.Location = new System.Drawing.Point(200, 200);
+            this.checkBoxTerms.Name = "checkBoxTerms";
+            this.checkBoxTerms.Size = new System.Drawing.Size(344, 23);
+            this.checkBoxTerms.TabIndex = 2;
+            this.checkBoxTerms.Text = "I accept the terms and conditions and privacy policy.";
+            this.checkBoxTerms.UseVisualStyleBackColor = true;
+            this.checkBoxTerms.CheckedChanged += new System.EventHandler(this.checkBoxTerms_CheckedChanged);
+
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(125, 75);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(450, 45);
+            this.labelTitle.TabIndex = 3;
+            this.labelTitle.Text = "Welcome to the Ultimate Game!";
+
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 192, 192);
-            ClientSize = new Size(944, 681);
-            Controls.Add(checkBox1);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            DoubleBuffered = true;
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelMain);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Game Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-
         #endregion
-        private Button button1;
-        private Button button2;
-        private CheckBox checkBox1;
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.CheckBox checkBoxTerms;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Panel panelMain;
     }
 }
